6f63d9c7c5a792642c05b3d669357472a051c760
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-08 14:54:36 -0700

    next commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - compulsory function. This would printa shot sentence.
+ *
+ * return returns 0
+ */
+
+int main(void)
+{
+	char statement = "\"Programming is like building a multilingual puzzle";
+	puts(statement\n);
+	return (0);
+}
