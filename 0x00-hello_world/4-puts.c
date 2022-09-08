7c00b10d40113d7a73fb930e1bf1ccac75165c93
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-08 15:09:44 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	char statement = "\"Programming is like building a multilingual puzzle";
-	puts(statement\n);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
