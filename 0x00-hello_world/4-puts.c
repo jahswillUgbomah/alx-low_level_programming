b8da633f488beedf9f08c767948f58657d0a7d1d
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-10 04:34:48 -0700

    correction on betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - compulsory function. This would printa shot sentence.
- * return: 0 if properly run. non-zero otherwise
+ * return: return 0 if properly nun. non-zero otherwise
  */
 int main(void)
 {
