d33ada044f78b58812629cb9211cc3208e4b740a
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-10 08:56:58 -0700

    good to go

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - to print out a statement
- * return 0 if it prints properly or non-zero otherwise
+ * Return: return 0 if it prints properly or non-zero otherwise
  */
 
 int main(void)
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - to show sizes of data types
- * return: 0 if properly run, non-zero otherwise
+ * Return: 0 if properly run, non-zero otherwise
  */
 
 int main(void)
