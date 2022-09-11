d3449a5ca1323be0168ac9bf1fdf49a09c49ec2a
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-11 14:13:49 -0700

    try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char sentence ="and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n]";
+	char sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	putchar(sentence);
 	return (0);
 }
