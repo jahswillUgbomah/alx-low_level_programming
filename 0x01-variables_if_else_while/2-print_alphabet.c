2336ea397427565976a55df119c49eb29ef723e2
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-11 15:26:20 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the characters a to z
+ * Return:  returns 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
