53eec9da4f1912959f4490af0d357171abfa3732
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-12 14:34:27 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - main function
- * Return: return 0 always
+ * Return: return 1
  */
 int main(void)
 {
-	char sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	putchar(sentence);
-	return (0);
+	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
