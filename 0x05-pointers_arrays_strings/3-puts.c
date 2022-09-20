dff42d9bda789f50e4db9be616167a08201599a3
Jahswill Ugbomah <jahswillugbomah@gmail.com>
2022-09-20 11:03:34 -0700

    commits

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the parameter to check
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_puts(str[i]);
+	}
+	_puts('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: parameter
+ */
+void print_rev(char *s)
+{
+	int a, b, len;
+
+	for (a = 0; srt[a] != '\0' a++)
+	{
+		len = i;
+	}
+	for (j = len - 1; j >= 0 j++)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+
+}
